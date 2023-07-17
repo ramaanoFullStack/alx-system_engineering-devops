89f9ae79e7647923e42b2fef1d9d63459164aedc
Ramaano <61529495+ramaanoFullStack@users.noreply.github.com>
2023-07-17 22:00:02 +0200

    Delete 0x1B-web_stack_debugging_4 directory

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++
@@ -1,6 +0,0 @@
-exec { 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 5000\"/g" /etc/default/nginx':
-  path => '/usr/bin:/usr/sbin:/bin',
-}
--> exec {'restart service':
-  command => '/usr/sbin/service nginx restart',
-}
