f3e6e44563eb28cedf85b8e743f9081a4f26f008
ramaanoFullStack <rami.stalin.m@outlook.com>
2023-08-09 00:37:15 +0000

    a function that queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,16 +12,16 @@ def number_of_subscribers(subreddit):
     (not active users, total subscribers) for a given subreddit.
     """
 
-if subreddit is None or not isinstance(subreddit, str):
-return 0
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
 
-user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-response = get(url, headers=user_agent)
-all_data = response.json()
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    all_data = response.json()
 
-try:
-return all_data.get('data').get('subscribers')
+    try:
+        return all_data.get('data').get('subscribers')
 
-except:
-return 0
+    except:
+        return 0
